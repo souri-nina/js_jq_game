1c94f7f6c4b59404832238abad59200876c462f5
souri.nina@gmail.com <snmoukdahan@gmail.com>
2018-08-24 15:14:25 -0600

    added vars

--- main.js
+++ main.js
@@ -4,12 +4,35 @@
 // allow exit or quit game option 
 // player correct guess = game finished
 
-var words = [
-  'devpoint',
-  'howdy',
-  'hangman',
-  'gameover'
-];
+var words = [ 'eclair', 'frapps', 'banana', 'shakes'];
+var user,
+    hangman,
+    letter
 
-var answer = words[Math.floor(Math.random() * words.length)];
+var playword = words[Math.floor(Math.random() * words.length)];
 
+var answerArray = [];
+    for (var i = 0; i < answer.length; i++) {
+      answerArray[i] = "_";
+    }
+    var remLetters = answer.length;
+
+    while (remainingLetters > 0) {
+      alert(answerArray.join(" "));
+
+      var guess = prompt("Guess a letter, or click Cancel to stop playing.");
+      if (guess === null) {
+        // Exit the game loop
+        break;
+      } else if (guess.length !== 1) {
+        alert("Please enter a single letter.");
+      } else {
+        // Update the game state with the guess
+        for (var j = 0; j < word.length; j++) {
+          if (word[j] === guess) {
+            answerArray[j] = guess;
+            remainingLetters--;
+          }
+        }
+      }
+    }  
